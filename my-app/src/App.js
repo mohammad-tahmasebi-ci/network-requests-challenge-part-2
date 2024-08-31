0eb5fa04a5103dde29e4eaf9a87d36e069d38863
Mohammad Tahmasebi <mohammad.tahmasebi@students.codeinstitute.net>
2024-08-31 22:17:42 +0100

    Update App.js
    update App.js to reference the relevant component, ContentAPI.js, as well the required import statement

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import css from './App.module.css';
 import Sidebar from "./components/Sidebar";
+import ContentAPI from './components/ContentAPI';
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
       <Sidebar />
+      <ContentAPI />
     </div>
   );
 }
