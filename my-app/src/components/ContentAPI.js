6713a9ca2869cb499d4320b12402a4aaea074ac0
Mohammad Tahmasebi <mohammad.tahmasebi@students.codeinstitute.net>
2024-08-31 22:14:33 +0100

    Create ContentAPI.js
    create the required ContentAPI.js to access 100 images from the Pixabah site using the API key

---
+++ my-app/src/components/ContentAPI.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import axios from "axios";
+import pixabay from '../pixabay.json'
+import API_KEY from '../secrets'
+import PostItemAPI from "./PostItemAPI";
+
+class ContentAPI extends Component {
+
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       posts: [],
+       isLoaded: false,
+       filteredPosts: [],
+    }
+  }
+
+    componentDidMount() {
+      this.fetchImages();
+    }
+
+   async fetchImages() {
+      const response = 
+        await axios.get('https://pixabay.com/api/?key=45663614-e00a7fbe886c1af81dbbfb94f&per_page=100'); 
+      const fetchedPosts = Array.isArray(response.data.hits) 
+          ? response.data.hits 
+          : [response.data.hits] 
+      this.setState({ posts: fetchedPosts });
+      this.setState({ isLoaded: true });  
+    }
+
+  handelChange = (event) => {
+    const inputText = event.target.value.toLowerCase()
+    const filteredPosts = this.state.posts.filter(post => {
+        return post.type.toLowerCase().includes(inputText)
+    })
+    this.setState({
+        filteredPosts: filteredPosts
+    })
+
+  }
+
+  render() {
+  return (
+    <div>
+        <input type='text' id="search" name='search' placeholder='Artwork type: photo' onChange={this.handelChange} />
+        {
+        this.state.filteredPosts.map(post => 
+        <form key={post.id}>
+          <PostItemAPI apost={post} />
+          <hr />
+        </form>
+      )}
+    </div>
+  )}
+}
+export default ContentAPI
