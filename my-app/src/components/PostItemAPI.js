416e2eb2c7bf796fb90d9f13fcf53296f500b7dc
Mohammad Tahmasebi <mohammad.tahmasebi@students.codeinstitute.net>
2024-08-31 22:15:52 +0100

    Create PostItemAPI.js
    create the component, PostItemAPI.js, to step through the images of the type selected in the search input box

---
+++ my-app/src/components/PostItemAPI.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function PostItemAPI(props) {
+  const {id, user, type, tags, webformatURL} = props.apost;
+  const artwork = `Artwork type: ${type}`;
+  const auser = `Artist: ${user}`;
+  const atag = `Tags: ${tags}`;
+
+  return (
+    <div key={id}>
+        <input type='text' name='artist' placeholder={auser} />
+        <br />
+        <img src={webformatURL} alt='some image'/>
+        <p>{atag}</p>
+        <br />
+    </div>
+  )
+}
+
+export default PostItemAPI
